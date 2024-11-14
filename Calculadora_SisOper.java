10cfede4750e271eef28acc439e6b2ac0c3452d2
Rhasall <rhasall.niper@gmail.com>
2024-11-14 20:50:54 +0100

    añadida la funcion potencia fixed#3

--- Calculadora_SisOper.java
+++ Calculadora_SisOper.java
@@ -13,12 +13,13 @@ public class Calculadora_SisOper
             System.out.println("3. Multiplicación");
             System.out.println("4. División");
             System.out.println("5. Modulo");
-            System.out.println("6. Salir");
+            System.out.println("6. Potencia");
+            System.out.println("7. Salir");
             System.out.print("Elige una opción: ");
 
             int opcion = scanner.nextInt();
 
-            if (opcion >= 1 && opcion <= 5) {
+            if (opcion >= 1 && opcion <= 6) {
                 System.out.print("Ingresa el primer número: ");
                 double num1 = scanner.nextDouble();
                 System.out.print("Ingresa el segundo número: ");
@@ -47,9 +48,17 @@ public class Calculadora_SisOper
                         } else {
                             System.out.println("Error: No se puede realizar la operacion.");
                         }
-                        break;    
+                        break; 
+                        
+                     case 6:  
+                         if (num2 != 0) {
+                            System.out.println("El resultado de elevar " + num1 + " a " + num2 +"da como resultado: " + potencia(num1, num2));
+                        } else {
+                            System.out.println("Error: No se puede realizar la operacion.");
+                        }
+                        break; 
                 }
-            } else if (opcion == 6) {
+            } else if (opcion == 7) { 
                 continuar = false;
                 System.out.println("Saliendo de la calculadora...");
             } else {
@@ -65,6 +74,7 @@ public class Calculadora_SisOper
     public static double multiplicar(double a, double b) { return a * b; }
     public static double dividir(double a, double b) { return a / b; }
     public static double resto(double a, double b) { return a % b; }
+    public static double potencia(double a, double b) { return Math.pow(a, b); }
 }
 
 
